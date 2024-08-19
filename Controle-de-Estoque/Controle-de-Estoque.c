31f7acdf1f6f9760129699ff892ed32638b3318e
Savio Henrique <saviobarradas@gmail.com>
2024-08-19 19:32:57 -0300

    utilização de struct

--- Controle-de-Estoque/Controle-de-Estoque.c
+++ Controle-de-Estoque/Controle-de-Estoque.c
@@ -1,82 +1,143 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define N 10
+typedef struct {
+	int codigo;
+	char descricao[100];
+	float preco;
+	int quantidade;
+}Produto;
 
-char descricao[N][100];
-float preco[N];
-int qtd[N];
-int id[N];
-int cont = 0;
+typedef struct {
+	Produto produtos[100];
+	int qtd; //Quantidade de produtos cadastrados
+}Estoque;
 
-void cadastrar();
-void listar();
+void cadastrar(Estoque *estoque);
+void listar(Estoque *estoque);
+void excluir(Estoque *estoque);
+char menu();
 
 void main(){
-	int menu;
-	do{
-		system("cls");
-		
-		printf("==== ESTOQUE FELIZ ====\n");
-		printf("\n1 - Cadastrar\n");
-		printf("2 - Listar\n");
-		printf("0 - Sair\n");
-		printf(">>> ");
-		scanf("%d", &menu);
-		switch(menu){
-			case 1:
-				cadastrar();
-				break;
-			case 2:
-				listar();
-				break;
-			default:
-				break;
-				
-		}
-		
-	}while(menu != 0);
-}
+    Estoque estoque;
+    estoque.qtd = 0;
 
-void cadastrar(){
-	fflush(stdin); // resolver bug de buffer do teclado na linguagem c
-	printf("---- CADASTRO DE PRODUTOS ----\n");
-	if(cont < N){
-		char d[100];
-		float p;
-		int q;
-		printf("\nDESCRICAO: ");
-		gets(d);
-		printf("PRECO: ");
-		scanf("%f", &p);
-		printf("QUANTIDADE: ");
-		scanf("%d", &q);
-		
-		preco[cont] = p;
-		qtd[cont] = q;
-		strcpy(descricao[cont], d);
-		id[cont] = cont + 1;
-		cont++;
-		printf("\nCadastrado com sucesso.\n");
-		
-	}else{
-		printf("Memoria cheia");
-	}
-	system("pause");
-	
-}// fim cadastrar
+    while(1){
+        fflush(stdin);
+        char op = menu();
+        if(op == '0'){
+            break;
+        }
+        switch(op){
+            case '1':
+                cadastrar(&estoque);
+                system("pause");
+                break;
+            case '2':
+                listar(&estoque);
+                system("pause");
+                break;
+            case '3':
+                excluir(&estoque);
+                system("pause");
+                break;
+            default:
+                printf("\nOpcao Invalida\n");
+                system("pause");
+        }
+    }//fim WHILE
 
-void listar(){
+}// Fim do MAIN
+
+char menu(){
+    system("cls");
+    char op;
+    printf("======= CONTROLE DE ESTOQUE =======\n\n");
+    printf("1 - CADASTRAR\n");
+    printf("2 - LISTAR\n");
+    printf("3 - EXCLUIR\n");
+    printf("0 - SAIR\n");
+    printf(">>> ");
+    scanf("%c", &op);
+    return op;
+
+}//fim MENU
+
+void cadastrar(Estoque *estoque){
 	system("cls");
-	int i;
-	printf("---- LISTAGEM DE PRODUTOS ----\n");
-	for(i=0;i<cont;i++){
-		printf("ID: %d\n", id[i]);
-		printf("DESCRICAO: %s\n", descricao[i]);
-		printf("PRECO: R$%.2f\n", preco[i]);
-		printf("QUANTIDADE: %d\n", qtd[i]);
-		printf("------------------------------------\n");
+	fflush(stdin);
+	char descricao[100];
+	float preco;
+	int qtd;
+
+	printf("======= CADASTRAR =======\n\n");
+	printf("DESCRICAO: ");
+	gets(descricao);
+	printf("PRECO: R$ ");
+	scanf("%f",&preco);
+	printf("QUANTIDADE: ");
+	scanf("%d",&qtd);
+
+	int codigo;
+	if(estoque->qtd == 0){
+		codigo = 1;
+	}else{
+		codigo = estoque->produtos[estoque->qtd-1].codigo + 1;
 	}
-	system("pause");
-}// fim listar
+
+	estoque->produtos[estoque->qtd].preco = preco;
+	estoque->produtos[estoque->qtd].quantidade = qtd;
+	strcpy(estoque->produtos[estoque->qtd].descricao, descricao);
+	estoque->produtos[estoque->qtd].codigo = codigo;
+	estoque->qtd++;
+
+	printf("\nProduto cadastrado com sucesso\n");
+
+}//fim cadastrar
+
+void listar(Estoque *estoque){
+    system("cls");
+    printf("======= LISTA DE PRODUTOS =======");
+    int i;
+    for(i=0; i<estoque->qtd;i++){
+        printf("\nCODIGO: %d", estoque->produtos[i].codigo);
+        printf("\nDESCRICAO: ");
+        puts(estoque->produtos[i].descricao);
+        printf("PRECO: R$ %.2f", estoque->produtos[i].preco);
+        printf("\nQUANTIDADE: %d", estoque->produtos[i].quantidade);
+        printf("\n--------------------------\n");
+    }
+}//fim listar
+
+void excluir(Estoque *estoque){
+    system("cls");
+    printf("======= EXCLUIR =======");
+    listar(estoque);
+    int c;
+    printf("Qual produto deseja excluir?: ");
+    scanf("%d", &c);
+    int ctz = 0;
+    printf("Certeza que deseja exlcuir o produto %d (1 - SIM; 2 - NAO):", c);
+    scanf("%d", &ctz);
+    if(ctz == 1){
+        int i, j, encontrado = 0;
+        for(i = 0; i < estoque->qtd; i++){
+            if(estoque->produtos[i].codigo == c){
+                for(j = i; j < estoque->qtd - 1; j++){
+                    estoque->produtos[j] = estoque->produtos[j + 1];
+                }
+                estoque->qtd--;
+                encontrado = 1;
+                break;
+            }
+        }
+        if(encontrado == 0){
+            printf("\nProduto não encontrado!");
+        }else{
+            printf("\nProduto exlcuido com sucesso!");
+        }
+        printf("\n");
+
+    }//fim IF certeza
+}//fim EXCLUIR
